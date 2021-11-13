e89ad17079d4097035664b5938078a73f97ded74
David Barthelemy <davidbarthelemy28@gmail.com>
2021-11-13 13:16:39 +0100

    update TODO

--- flashdb/__init__.py
+++ flashdb/__init__.py
@@ -1,7 +1,18 @@
 """
 TODO:
-    - Define a grammar
-    - Create a parser for json query
-    - Create an error handler and test
-    - Create the optimizer and test => Do I need it? For what? Check what the mysql optimizer do
+- Create Functions for select
+    - update the parser method
+    - apply_functions parser error
+    - unit test
+
+- Create Filters for where
+    - update the parser method
+    - Parser Error
+    - unit test
+
+- Add ASGI and start recevied the first select
+    - Start running server
+        - Load info like storage, existing index, ...
+        - accept read request
+    - Unit test
 """
